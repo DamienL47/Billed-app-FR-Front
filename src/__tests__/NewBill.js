585ef09a51aef9b8984c930cac255ffc355938d7
DamienL47 <damien.lataste@lapiscine.pro>
2023-08-15 14:50:24 +0200

    fix test NewBill

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -5,47 +5,46 @@ import '@testing-library/jest-dom';
 
 import { screen, fireEvent } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
-import { mockedBills, list } from "../__mocks__/store.js"
+import { list } from "../__mocks__/store.js"
 import NewBill from "../containers/NewBill.js"
-
-// mock de localStorage
 import { localStorageMock } from "../__mocks__/localStorage.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then file input should trigger handleChangeFile on file selection", async () => {
+    test("Then file input should trigger handleChangeFile on file selection", () => {
       const html = NewBillUI();
       document.body.innerHTML = html;
 
-      // Mock de la classe NewBill
+      // Création d'un objet NewBill
+      const onNavigateMock = jest.fn();
       const newBillMock = new NewBill({
         document,
-        onNavigate: jest.fn(),
-        store: {bills: list},         
+        onNavigate: onNavigateMock,
+        store: { bills: list },
         localStorage: localStorageMock,
       });
 
-      const eventMock = { preventDefault: jest.fn()}
-
-      // Mock de la fonction handleChangeFile
-      const handleChangeFileMock = jest.fn(newBillMock.handleChangeFile(eventMock));
+      // Mock de la méthode handleChangeFile
+      const eventMock = { preventDefault: jest.fn() };
+  
+      // Espionner la méthode handleChangeFile
+      const handleChangeFileSpy = jest.spyOn(newBillMock, 'handleChangeFile');
 
       // Récupération de l'input file
       const fileInput = screen.getByTestId("file");
 
+      // Mock de l'objet de fichier (simulant la sélection de fichier)
+      const file = new File(['test'], 'image.jpg', { type: 'image/jpeg' });
+      Object.defineProperty(fileInput, 'files', { value: [file] });
+
       // Simulation de la sélection de fichier
-      fireEvent.change(fileInput, {
-        target: {
-          files: [new File([''], 'test.png', { type: 'image/png' })],
-        },
-      });
+      fireEvent.change(fileInput);
 
-      // Utilisation de await pour attendre la résolution de la promesse
-      await handleChangeFileMock();
+      // Appel de la méthode handleChangeFile
+      newBillMock.handleChangeFile(eventMock);
 
-      // Vérification de l'appel de la fonction handleChangeFile
-      expect(handleChangeFileMock).toHaveBeenCalled();
+      // Vérification de l'appel de la fonction
+      expect(handleChangeFileSpy).toHaveBeenCalled();
     });
-
-  })
+  });
 });
