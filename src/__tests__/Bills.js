688c86d47dfe65007f9b2948320805bd3d1cd217
DamienL47 <damien.lataste@lapiscine.pro>
2023-09-15 12:10:56 +0200

    fix test error 404 and 500

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -4,15 +4,19 @@
 
 import '@testing-library/jest-dom';
 
-import {screen, waitFor, fireEvent } from "@testing-library/dom"
-import BillsUI from "../views/BillsUI.js"
-import { bills } from "../fixtures/bills.js"
+import {screen, waitFor, fireEvent } from "@testing-library/dom";
+import BillsUI from "../views/BillsUI.js";
+import Bills from '../containers/Bills.js';
+import { bills } from "../fixtures/bills.js";
+import store from '../__mocks__/store';
+import Store from '../app/Store.js'
+import mockStore from "../__mocks__/store"
 import { ROUTES_PATH } from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
-import { formatDate } from '../app/format.js';
 import router from "../app/Router.js";
-import Bills from '../containers/Bills.js';
-import { sortByDate } from '../containers/Bills.js'
+
+jest.mock("../app/store", () => mockStore)
+
 
 // Fonction pour convertir une date au format "DD-MMM-YY" en "YYYY-MM-DD"
 const convertDateToYYYYMMDD = (dateStr) => {
@@ -151,6 +155,55 @@ describe("Given I am connected as an employee", () => {
         // Expect sortByDate to have been called
         expect(sortByDateSpy).toHaveBeenCalled();
       });
+      describe("Given I am a user connected as Employee", () => {
+   
+        describe("When an error occurs on API", () => {
+          beforeEach(() => {
+            jest.spyOn(mockStore, "bills")
+            Object.defineProperty(
+                window,
+                'localStorage',
+                { value: localStorageMock }
+            )
+            window.localStorage.setItem('user', JSON.stringify({
+              type: 'Employee',
+              email: "a@a"
+            }))
+            const root = document.createElement("div")
+            root.setAttribute("id", "root")
+            document.body.appendChild(root)
+            router()
+          })
+          test("fetches bills from an API and fails with 404 message error", async () => {
+      
+            mockStore.bills.mockImplementationOnce(() => {
+              return {
+                list : () =>  {
+                  return Promise.reject(new Error("Erreur 404"))
+                }
+              }})
+            window.onNavigate(ROUTES_PATH.Bills)
+            await new Promise(process.nextTick);
+            const message = await screen.getByText(/Erreur 404/)
+            expect(message).toBeTruthy()
+          })
+      
+          test("fetches messages from an API and fails with 500 message error", async () => {
+      
+            mockStore.bills.mockImplementationOnce(() => {
+              return {
+                list : () =>  {
+                  return Promise.reject(new Error("Erreur 500"))
+                }
+              }})
+      
+            window.onNavigate(ROUTES_PATH.Bills)
+            await new Promise(process.nextTick);
+            const message = await screen.getByText(/Erreur 500/)
+            expect(message).toBeTruthy()
+          })
+        })
+      })
     });
   });
 })
